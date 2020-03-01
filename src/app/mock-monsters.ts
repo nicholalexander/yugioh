098ff3711e0da83d430cb509445699e08564581f
nichol alexander <nichol.alexander@gmail.com>
2020-03-01 12:33:32 -0500

    mocks, list, show details

---
+++ src/app/mock-monsters.ts
@@ -0,0 +1,10 @@
+import { Monster } from './monster';
+
+export const MONSTERS: Monster[] = [
+    { id: 1, name: 'Baby Dragon', attack: 1200, defense: 700 },
+    { id: 2, name: 'First of the Dragons', attack: 2700, defense: 2000},
+    { id: 3, name: 'Obelisk The Tormentor', attack: 4000, defense: 4000},
+    { id: 4, name: 'Blue-Eyes White Dragon', attack: 3000, defense: 2500},
+    { id: 5, name: 'Alexandrite Dragon', attack: 2000, defense: 100},
+    { id: 6, name: 'Red-Eyes Black Flare Dragon', attack: 2400, defense: 2000},
+]
--- src/app/monsters/monsters.component.ts
+++ src/app/monsters/monsters.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Monster } from '../monster';
 
+import {MONSTERS} from '../mock-monsters';
+
 @Component({
   selector: 'app-monsters',
   templateUrl: './monsters.component.html',
@@ -16,6 +18,16 @@ export class MonstersComponent implements OnInit {
     attack: 1200,
     defense: 700
   };
+
+  monsters = MONSTERS;
+
+  selectedMonster: Monster;
+  
+  onSelect(monster: Monster): void {
+    this.selectedMonster = monster;
+    console.log("you click-ed a monster!");
+    console.log(monster);
+  }
 
   ngOnInit() {
   }
